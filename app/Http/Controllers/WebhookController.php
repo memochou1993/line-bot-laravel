5b2d71e8436bcbbe0c9998112b9689107478544b
Memo Chou <memochou1993@gmail.com>
2023-07-22 23:43:57 +0800

    Upgrade linecorp/line-bot-sdk dependency

--- app/Http/Controllers/WebhookController.php
+++ app/Http/Controllers/WebhookController.php
@@ -2,45 +2,60 @@
 
 namespace App\Http\Controllers;
 
-use Exception;
+use GuzzleHttp\Client;
 use Illuminate\Http\Request;
-use LINE\LINEBot;
-use LINE\LINEBot\Constant\HTTPHeader;
-use LINE\LINEBot\Event\MessageEvent\TextMessage;
-use LINE\LINEBot\HTTPClient\CurlHTTPClient;
+use LINE\Clients\MessagingApi\Api\MessagingApiApi;
+use LINE\Clients\MessagingApi\Configuration;
+use LINE\Clients\MessagingApi\Model\ReplyMessageRequest;
+use LINE\Clients\MessagingApi\Model\TextMessage;
+use LINE\Constants\HTTPHeader;
+use LINE\Constants\MessageType;
+use LINE\Parser\EventRequestParser;
+use LINE\Parser\Exception\InvalidEventRequestException;
+use LINE\Parser\Exception\InvalidSignatureException;
+use LINE\Webhook\Model\MessageEvent;
+use LINE\Webhook\Model\TextMessageContent;
 use Symfony\Component\HttpFoundation\Response;
 
 class WebhookController extends Controller
 {
-    function handleEvents(Request $request) {
-        $channelToken = env('LINE_CHANNEL_ACCESS_TOKEN');
-        $channelSecret = env('LINE_CHANNEL_SECRET');
-
-        $httpClient = new CurlHTTPClient($channelToken);
-        $bot = new LINEBot($httpClient, ['channelSecret' => $channelSecret]);
+    public function __invoke(Request $request)
+    {
+        $config = new Configuration();
+        $config->setAccessToken(env('LINE_CHANNEL_ACCESS_TOKEN'));
+        $client = new MessagingApiApi(new Client(), $config);
 
         $signature = $request->header(HTTPHeader::LINE_SIGNATURE);
         if (!$signature) {
-            return response()->json(null, Response::HTTP_BAD_REQUEST);
+            abort(Response::HTTP_BAD_REQUEST);
         }
 
         try {
-            $events = $bot->parseEventRequest($request->getContent(), $signature);
-        } catch (Exception) {
-            return response()->json(null, Response::HTTP_BAD_REQUEST);
+            $secret = env('LINE_CHANNEL_SECRET');
+            $parsedEvents = EventRequestParser::parseEventRequest($request->getContent(), $secret, $signature);
+        } catch (InvalidSignatureException) {
+            abort(Response::HTTP_BAD_REQUEST);
+        } catch (InvalidEventRequestException) {
+            abort(Response::HTTP_BAD_REQUEST);
         }
 
-        collect($events)
-            ->filter(fn ($event) => $event instanceof TextMessage)
-            ->each(function ($event) use ($bot) {
-                $replyText = $event->getText();
-                try {
-                    $bot->replyText($event->getReplyToken(), $replyText);
-                } catch (Exception) {
-                    response()->json(null, Response::HTTP_INTERNAL_SERVER_ERROR);
-                }
+        collect($parsedEvents->getEvents())
+            ->filter(fn ($event) => $event instanceof MessageEvent)
+            ->filter(fn ($event) => $event->getMessage() instanceof TextMessageContent)
+            ->each(function ($event) use ($client) {
+                $replyText = $event->getMessage()->getText();
+
+                $client->replyMessage(new ReplyMessageRequest([
+                    'replyToken' => $event->getReplyToken(),
+                    'messages' => [
+                        new TextMessage([
+                            'type' => MessageType::TEXT,
+                            'text' => $replyText,
+                        ]),
+                    ],
+                ]));
             });
 
-        return response()->json([], Response::HTTP_OK);
+        return response()->json(null);
     }
 }
--- routes/api.php
+++ routes/api.php
@@ -3,4 +3,4 @@
 use App\Http\Controllers\WebhookController;
 use Illuminate\Support\Facades\Route;
 
-Route::post('/webhook', [WebhookController::class, 'handleEvents']);
+Route::post('/webhook', WebhookController::class);
